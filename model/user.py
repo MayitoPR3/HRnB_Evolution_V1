67a15dfdf096da91984eb1172185a5ca8d062ab7
Jose Mayo <josemayo3@gmail.com>
2024-06-07 13:26:33 -0400

    updated the User file

--- model/user.py
+++ model/user.py
@@ -1,17 +1,26 @@
+from model.base_model import Basemodel
 
-class User:
-    def __init__(self, user_id, first_name, last_name, password):
-        
-        self.user_id = user_id
+
+class User(Basemodel):
+    """class for the User
+    """
+    def __init__(self, email, password, first_name="", last_name="", **kwargs):
+        """initializes the function of user information"""
+        super().__init__(**kwargs)
         self.first_name = first_name
         self.last_name = last_name
-        self.email = first_name + '.' + last_name + '@gmail.com'
+        self.email = email
         self.password = password
         self.places = []
         self.reviews = []
-        self.city = None
-        self.country = None
-
+        
+    def update_name(self, first_name="", last_name=""):
+        """ defines the process to update the name"""
+        self.first_name = first_name
+        self.last_name = last_name
+        
+    def __str__(self):
+        return f"[User] ({self.id}) {self.to_dict()}"
     
     
 
