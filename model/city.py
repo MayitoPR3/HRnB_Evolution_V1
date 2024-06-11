ccc8d6b5c32d11648f8f8226ee012ab21dac41fb
Jose Mayo <josemayo3@gmail.com>
2024-06-11 14:42:15 -0400

    added documentation

--- model/city.py
+++ model/city.py
@@ -3,6 +3,7 @@ from model.base_model import Basemodel
 
 
 class City(Basemodel):
+    """this is a city class that represents a city"""
     def __init__(self, city_name, country_code, place_name):
         super().__init__()
         self.city_name = city_name
@@ -10,8 +11,10 @@ class City(Basemodel):
         self.place_name = place_name
     
     def add_city(self, city_name):
+        """"Add a new city"""
         self.city_name.append(city_name)
         self.place_name.append(city_name)
         
     def __str__(self):
+        """returns a string representation of the city"""
         return f"[City] ({self.id}) {self.to_dict()}"
