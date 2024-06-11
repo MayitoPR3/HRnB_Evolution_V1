269f4a5f7c9289dd023c556c23b8191237e78d3b
Jose Mayo <josemayo3@gmail.com>
2024-06-10 23:28:50 -0400

    update reviews

--- model/reviews.py
+++ model/reviews.py
@@ -1,8 +1,11 @@
+from model.base_model import Basemodel
 
-class Reviews:
-    def __init__(self, user_feedback, place_rating):
-        
-        self.user_feedback = user_feedback
-        self.place_rating = place_rating
 
-    
+class Review(Basemodel):
+    def __init__(self, comment, rating):
+        super().__init__()  # Call the __init__ method of the BaseModel class
+        self.comment = comment
+        self.rating = rating
+
+    def __str__(self):
+        return f"[Review] ({self.id}) {self.to_dict()}"
