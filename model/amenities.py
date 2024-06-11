1c47387f9632c9a7211209a5692f95e9098efeee
Jose Mayo <josemayo3@gmail.com>
2024-06-11 14:40:26 -0400

    added documentation

--- model/amenities.py
+++ model/amenities.py
@@ -2,6 +2,7 @@
 from model.base_model import Basemodel
 
 class Amenities(Basemodel):
+    """this class represents the Amenities"""
     def __init__(self, name, description):
         super().__init__()
         self.name = name
