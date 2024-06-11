a49f6011e5f9c5b607f8ae247bd8c0eafca7a009
Jose Mayo <josemayo3@gmail.com>
2024-06-11 14:38:25 -0400

    added documentation

--- tests/test_amenity.py
+++ tests/test_amenity.py
@@ -4,12 +4,14 @@ from model.amenities import Amenities
 
 class TestAmenities(unittest.TestCase):
     def test_create_amenity(self):
+        """test creating an amenity"""
         amenity = Amenities(name="Wi-Fi", description="HIgh Speed")
         self.assertEqual(amenity.name, "Wi-Fi")
         self.assertEqual(amenity.description, "HIgh Speed")
         self.assertEqual(type(amenity), Amenities)
         
     def test_create_amenity_without_description(self):
+        """test create an amenity without description"""
         amenity = Amenities(name="Wi-Fi", description=None)
         self.assertEqual(amenity.name, "Wi-Fi")
         self.assertEqual(amenity.description, None)
