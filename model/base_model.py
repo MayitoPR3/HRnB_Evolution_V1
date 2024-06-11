13c05d3327ff80ab8f1a1d4bf0cff1bb77648539
Jose Mayo <josemayo3@gmail.com>
2024-06-11 00:36:15 -0400

    update

--- model/base_model.py
+++ model/base_model.py
@@ -9,7 +9,6 @@ class Basemodel:
     def __init__(self, id=None, created_at=None, updated_at=None):
         """initialization of the class
 
-        Args:
             id or uuid : creates a UUID4 for every process
             updated_at : created the updated time of the request
             created_at : creates the creation time of the request
@@ -17,9 +16,9 @@ class Basemodel:
         self.id = id or str(uuid.uuid4())
         self.created_at = created_at or datetime.now()
         self.updated_at = updated_at or datetime.now()
-
+      
     def save(self):
         self.updated_at = datetime.now()
-        
+
     def to_dict(self):
         return self.__dict__
--- model/user.py
+++ model/user.py
@@ -15,11 +15,6 @@ class User(Basemodel):
         self.place_name = []
         self.comment = []
         
-    def update_name(self, first_name="", last_name=""):
-        """ defines the process to update the name"""
-        self.first_name = first_name
-        self.last_name = last_name
-        
     def __str__(self):
         return f"[User] ({self.id}) {self.to_dict()}"
     
